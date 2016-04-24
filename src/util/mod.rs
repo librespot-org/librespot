8bbed4c162e3759f447b0f256a96b70e65fa57a4
Paul Lietar <paul@lietar.net>
2016-04-24 09:50:55 +0100

    Remove unused eprintln macro and hex dump function.

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -16,34 +16,8 @@ pub use util::spotify_id::{SpotifyId, FileId};
 pub use util::arcvec::ArcVec;
 pub use util::subfile::Subfile;
 
-#[macro_export]
-macro_rules! eprintln(
-    ($($arg:tt)*) => (
-        {
-            use std::io::Write;
-            writeln!(&mut ::std::io::stderr(), $($arg)* ).unwrap()
-        }
-    )
-);
-#[macro_export]
-macro_rules! eprint(
-    ($($arg:tt)*) => (
-        {
-            use std::io::Write;
-            write!(&mut ::std::io::stderr(), $($arg)* ).unwrap()
-        }
-    )
-);
-
 pub fn rand_vec<G: Rng, R: Rand>(rng: &mut G, size: usize) -> Vec<R> {
     rng.gen_iter().take(size).collect()
-}
-
-pub fn hexdump(data: &[u8]) {
-    for b in data.iter() {
-        eprint!("{:02X} ", b);
-    }
-    eprintln!("");
 }
 
 pub trait IgnoreExt {
