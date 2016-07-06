facd6c58814a6a2d7b59db82ad182a02957b4f54
Paul Lietar <paul@lietar.net>
2016-07-06 01:34:20 +0100

    Update dependencies.
    This removes the direct dependency on syntex, meaning plugins can bump
    their own versions independently (see serde-rs/serde#358)

--- build.rs
+++ build.rs
@@ -1,20 +1,17 @@
 extern crate vergen;
-#[cfg(feature = "with-syntex")] extern crate syntex;
-#[cfg(feature = "with-syntex")] extern crate json_macros;
-#[cfg(feature = "with-syntex")] extern crate protobuf_macros;
 
 #[cfg(feature = "with-syntex")]
 fn codegen() {
+    extern crate json_macros;
+    extern crate protobuf_macros;
+
     use std::env;
     use std::path::PathBuf;
-    use std::path::Path;
 
-    let mut registry = syntex::Registry::new();
     let out = PathBuf::from(env::var("OUT_DIR").unwrap());
 
-    json_macros::plugin_registrar(&mut registry);
-    protobuf_macros::plugin_registrar(&mut registry);
-    registry.expand("librespot", Path::new("src/lib.in.rs"), &out.join("lib.rs")).unwrap();
+    json_macros::expand("src/lib.in.rs", &out.join("lib.tmp0.rs")).unwrap();
+    protobuf_macros::expand(&out.join("lib.tmp0.rs"), &out.join("lib.rs")).unwrap();
 }
 
 #[cfg(not(feature = "with-syntex"))]
