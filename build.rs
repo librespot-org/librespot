541114e2b4bd0335791bb6c5d945162f1884813e
Paul Lietar <paul@lietar.net>
2017-02-22 14:27:54 +0000

    build: only rerun syntex when relevant files have changed

--- build.rs
+++ build.rs
@@ -9,5 +9,9 @@ fn main() {
 
     let out = PathBuf::from(env::var("OUT_DIR").unwrap());
     protobuf_macros::expand("src/lib.in.rs", &out.join("lib.rs")).unwrap();
-}
 
+    println!("cargo:rerun-if-changed=src/lib.in.rs");
+    println!("cargo:rerun-if-changed=src/spirc.rs");
+    println!("cargo:rerun-if-changed=src/connection");
+
+}
