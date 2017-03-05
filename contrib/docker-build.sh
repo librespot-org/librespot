6daedde0e93887effe536e5ca904c0517073ea6d
Johan Anderholm <johan.anderholm@gmail.com>
2017-03-05 02:03:56 -0800

    Add mipsel as a cross-compilation target.

--- contrib/docker-build.sh
+++ contrib/docker-build.sh
@@ -4,3 +4,4 @@ set -eux
 cargo build --release --no-default-features --features alsa-backend
 cargo build --release --target arm-unknown-linux-gnueabihf --no-default-features --features alsa-backend
 cargo build --release --target arm-unknown-linux-gnueabi --no-default-features --features alsa-backend
+cargo build --release --target mipsel-unknown-linux-gnu --no-default-features --features alsa-backend
