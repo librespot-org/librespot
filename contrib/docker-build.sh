d01b7a47369b2725997c5d2cc4f175781532946c
Anton Voyl <awiouy@gmail.com>
2017-06-08 13:31:06 +0200

    Add aarch64 as a cross-compilation target (#194)

--- contrib/docker-build.sh
+++ contrib/docker-build.sh
@@ -2,6 +2,7 @@
 set -eux
 
 cargo build --release --no-default-features --features alsa-backend
+cargo build --release --target aarch64-unknown-linux-gnu --no-default-features --features alsa-backend
 cargo build --release --target arm-unknown-linux-gnueabihf --no-default-features --features alsa-backend
 cargo build --release --target arm-unknown-linux-gnueabi --no-default-features --features alsa-backend
 cargo build --release --target mipsel-unknown-linux-gnu --no-default-features --features alsa-backend
