aa86ebf549d6bb9937dcc3d7a54bd39f9e21defd
Paul Lietar <paul@lietar.net>
2017-07-09 20:01:14 +0200

    Add two new file formats to enumeration
    Fixes #212 and #213

--- protocol/files.rs
+++ protocol/files.rs
@@ -4,7 +4,7 @@ pub const FILES : &'static [(&'static str, u32)] = &[
     ("proto/authentication.proto", 2098196376),
     ("proto/keyexchange.proto", 451735664),
     ("proto/mercury.proto", 709993906),
-    ("proto/metadata.proto", 488967056),
+    ("proto/metadata.proto", 934494714),
     ("proto/pubsub.proto", 2686584829),
     ("proto/spirc.proto", 3618770573),
 ];
