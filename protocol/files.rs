696aec33e99641b63bf7048700ce4b2ef678ac11
Jakob <jakob.riepler@chaosfield.at>
2017-10-01 00:51:05 +0200

    Add album type 'EP' (#255)
    * add album type 'EP'
    
    * fix indentation

--- protocol/files.rs
+++ protocol/files.rs
@@ -4,7 +4,7 @@ pub const FILES : &'static [(&'static str, u32)] = &[
     ("proto/authentication.proto", 2098196376),
     ("proto/keyexchange.proto", 451735664),
     ("proto/mercury.proto", 709993906),
-    ("proto/metadata.proto", 934494714),
+    ("proto/metadata.proto", 3146937699),
     ("proto/pubsub.proto", 2686584829),
     ("proto/spirc.proto", 3618770573),
 ];
