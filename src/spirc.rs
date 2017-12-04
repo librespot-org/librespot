9e51977885afc5ca2c02ab71cedbc24ae9604ea7
Federico Fossemò <ghosty89@gmail.com>
2017-12-04 12:16:41 +0100

    reset volume to 50%

--- src/spirc.rs
+++ src/spirc.rs
@@ -142,7 +142,7 @@ impl Spirc {
 
         let (cmd_tx, cmd_rx) = mpsc::unbounded();
 
-        let volume = 0x3333;
+        let volume = 0x8000;
         let device = initial_device_state(config, volume);
         mixer.set_volume(volume);
 
