e3516ebd9baaeb48740e4c6f50aca1bae7e07463
awiouy <awiouy@gmail.com>
2018-02-06 01:00:00 +0100

    Move discovery to its own module.

---
+++ discovery/src/lib.rs
@@ -0,0 +1,22 @@
+#[macro_use] extern crate log;
+#[macro_use] extern crate serde_json;
+
+extern crate base64;
+extern crate crypto;
+extern crate futures;
+extern crate hyper;
+extern crate num_bigint;
+extern crate protobuf;
+extern crate rand;
+extern crate tokio_core;
+extern crate url;
+
+#[cfg(feature = "with-dns-sd")]
+extern crate dns_sd;
+
+#[cfg(not(feature = "with-dns-sd"))]
+extern crate mdns;
+
+extern crate librespot_core as core;
+
+pub mod discovery;
--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,6 @@
 #![cfg_attr(feature = "cargo-clippy", allow(unused_io_amount))]
 
 #[macro_use] extern crate log;
-#[macro_use] extern crate serde_json;
-#[macro_use] extern crate serde_derive;
 
 extern crate base64;
 extern crate crypto;
@@ -18,6 +16,7 @@ extern crate url;
 
 pub extern crate librespot_audio as audio;
 pub extern crate librespot_core as core;
+pub extern crate librespot_discovery as discovery;
 pub extern crate librespot_protocol as protocol;
 pub extern crate librespot_metadata as metadata;
 
@@ -36,14 +35,7 @@ extern crate jack;
 #[cfg(feature = "libc")]
 extern crate libc;
 
-#[cfg(feature = "with-dns-sd")]
-extern crate dns_sd;
-
-#[cfg(not(feature = "with-dns-sd"))]
-extern crate mdns;
-
 pub mod audio_backend;
-pub mod discovery;
 pub mod mixer;
 pub mod player;
 
