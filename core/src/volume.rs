47d1e54debd48dd9e9645538c4d8521c935958fc
bricedp <bricedp@users.noreply.github.com>
2018-05-11 18:14:37 -0400

    create Volume struct for use with Cache

---
+++ core/src/volume.rs
@@ -0,0 +1,40 @@
+use std::fs::File;
+use std::io::{Read, Write};
+use std::path::Path;
+
+
+pub struct Volume {
+    pub volume: i32,
+}
+
+impl Volume {
+    // read volume from file, enforce upper/lower bounds
+    // convert volume from 0..100 to 0..0xFFFF
+    fn from_reader<R: Read>(mut reader: R) -> Volume {
+        let mut contents = String::new();
+        reader.read_to_string(&mut contents).unwrap();
+        let volume = contents.trim().parse::<i32>().unwrap();
+        if volume < 0 || volume > 100 {
+            panic!("Cached volume must be in the range 0-100");
+        }
+        Volume {
+            volume: volume * 0xFFFF / 100
+        }
+    }
+
+    pub(crate) fn from_file<P: AsRef<Path>>(path: P) -> Option<Volume> {
+        File::open(path).ok().map(Volume::from_reader)
+    }
+
+    // convert volume from 0..0xFFFF to 0..100
+    // write to plaintext file
+    fn save_to_writer<W: Write>(&self, writer: &mut W) {
+        let volume = self.volume * 100 / 0xFFFF;
+        writer.write_all(volume.to_string().as_bytes()).unwrap();
+    }
+
+    pub(crate) fn save_to_file<P: AsRef<Path>>(&self, path: P) {
+        let mut file = File::create(path).unwrap();
+        self.save_to_writer(&mut file)
+    }
+}
