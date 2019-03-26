9f9218f3cc772dc24402f26695f34f51586c4d29
ashthespy <ashthespy@gmail.com>
2019-03-26 18:07:05 +0100

    Remove redundant `rpassword` crate from `librespot-core`

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -23,7 +23,6 @@ extern crate num_integer;
 extern crate num_traits;
 extern crate protobuf;
 extern crate rand;
-extern crate rpassword;
 extern crate serde;
 extern crate serde_json;
 extern crate shannon;
