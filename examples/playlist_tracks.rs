2c35069c4d72a96d267fff7aee9438dd6a25c5ca
HEnquist <henrik.enquist@gmail.com>
2019-08-05 23:42:16 +0200

    Add basic support for playlists

--- examples/playlist_tracks.rs
+++ examples/playlist_tracks.rs
@@ -1,10 +1,13 @@
+#[macro_use]
 extern crate log;
 extern crate env_logger;
 
 extern crate librespot;
 extern crate tokio_core;
+//extern crate tokio_fs;
 extern crate tokio_io;
 extern crate futures;
+//extern crate futures_cpupool;
 
 use std::env;
 use tokio_core::reactor::Core;
@@ -13,7 +16,39 @@ use librespot::core::authentication::Credentials;
 use librespot::core::config::SessionConfig;
 use librespot::core::session::Session;
 use librespot::core::spotify_id::SpotifyId;
-use librespot::metadata::{Metadata, Track, Playlist};
+use librespot::playback::config::PlayerConfig;
+use librespot::playback::config::Bitrate;
+use librespot::metadata::{FileFormat, Metadata, PlaylistMeta, Track, Album, Artist, Playlist};
+
+
+/*
+fn make_list_playlist(core: &mut Core, session: &Session, uri: &str) -> TrackList {
+    let mut tracks = Vec::new();
+    let mut fnames = Vec::new();
+
+    let plist_uri = SpotifyId::from_base62(&uri).unwrap();
+    let plist = core.run(Playlist::get(&session, plist_uri)).unwrap();
+    println!("album name: {}",plist.name);
+    let plist_name = &plist.name;
+
+    
+
+    for (i, track_id) in plist.tracks.iter().enumerate() {
+        let plist_track = core.run(Track::get(&session, *track_id)).unwrap();
+        //println!("album track: {} - {}",i+1, alb_track.name);
+        let artist = core.run(Artist::get(&session, plist_track.artists[0])).unwrap();
+        println!("track artist: {}",artist.name);
+        tracks.push(plist_track.id);
+        let filename = format!("{} - {}.ogg",&artist.name, alb_track.name);
+        fnames.push(filename);
+    }
+    let ntr = plist.tracks.len();
+
+    let folder = format!("{}",plist_name);
+    let mut tlist = TrackList::new(ntr, folder, tracks, fnames);
+    tlist
+}
+*/
 
 fn main() {
     env_logger::init();
@@ -21,16 +56,20 @@ fn main() {
     let handle = core.handle();
 
     let session_config = SessionConfig::default();
+    let mut player_config = PlayerConfig::default();
+    player_config.bitrate = Bitrate::Bitrate320;
 
     let args: Vec<_> = env::args().collect();
-    if args.len() != 4 {
-        println!("Usage: {} USERNAME PASSWORD PLAYLIST", args[0]);
+    if args.len() != 5 {
+        println!("Usage: {} USERNAME PASSWORD PLAYLIST PLISTUSER", args[0]);
     }
     let username = args[1].to_owned();
     let password = args[2].to_owned();
     let credentials = Credentials::with_password(username, password);
 
-    let uri_split = args[3].split(":");
+    let plist_owner = args[4].to_string();
+
+    let mut uri_split = args[3].split(":");
     let uri_parts: Vec<&str> = uri_split.collect();
     println!("{}, {}, {}",uri_parts[0], uri_parts[1], uri_parts[2]);
     
@@ -40,7 +79,7 @@ fn main() {
         .run(Session::connect(session_config, credentials, None, handle))
         .unwrap();
 
-    let plist = core.run(Playlist::get(&session, plist_uri)).unwrap();
+    let plist = core.run(Playlist::get(&session, plist_uri, plist_owner, 0, 100)).unwrap();
     println!("{:?}",plist);
     for track_id in plist.tracks {
         let plist_track = core.run(Track::get(&session, track_id)).unwrap();
--- metadata/src/lib.rs
+++ metadata/src/lib.rs
@@ -72,6 +72,29 @@ pub trait Metadata: Send + Sized + 'static {
     }
 }
 
+pub trait PlaylistMeta: Send + Sized + 'static {
+    type Message: protobuf::Message;
+
+    fn base_url() -> &'static str;
+    fn parse(msg: &Self::Message, session: &Session) -> Self;
+
+    fn get(session: &Session, id: SpotifyId, user: String, start: i32, len: i32) -> Box<Future<Item = Self, Error = MercuryError>> {
+        //let uri = format!("hm://playlist/{}?from={}&length={}",id.to_base62(), 0, 100);
+        let uri = format!("hm://playlist/user/{}/playlist/{}?from={}&length={}", user, id.to_base62(), start, len);
+        println!("request uri: {}", uri);
+        let request = session.mercury().get(uri);
+        println!("a");
+        let session = session.clone();
+        Box::new(request.and_then(move |response| {
+            println!("{:?}", response);
+            let data = response.payload.first().expect("Empty payload");
+            let msg: Self::Message = protobuf::parse_from_bytes(data).unwrap();
+            println!("{:?}", msg);
+            Ok(Self::parse(&msg, &session))
+        }))
+    }
+}
+
 #[derive(Debug, Clone)]
 pub struct Track {
     pub id: SpotifyId,
@@ -95,7 +118,7 @@ pub struct Album {
 
 #[derive(Debug, Clone)]
 pub struct Playlist {
-    pub user: String,
+    //pub id: SpotifyId,
     pub length: i32,
     pub name: String,
     pub tracks: Vec<SpotifyId>,
@@ -198,12 +221,13 @@ impl Metadata for Album {
     }
 }
 
-impl Metadata for Playlist {
+impl PlaylistMeta for Playlist {
     type Message = protocol::playlist4changes::SelectedListContent;
 
-    fn request_url(id: SpotifyId) -> String {
-        format!("hm://playlist/v2/playlist/{}", id.to_base62())
+    fn base_url() -> &'static str {
+        "hm://playlist/'?from=' + from + '&length=' + length"
     }
+
 
     fn parse(msg: &Self::Message, _: &Session) -> Self {
 
@@ -224,7 +248,6 @@ impl Metadata for Playlist {
             name: msg.get_attributes().get_name().to_owned(),
             length: msg.get_length(),
             tracks: tracks,
-            user: msg.get_owner_username().to_string(),
         }
     }
 }
