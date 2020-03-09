6557055f319e2cda370df543619d7b858aec0b5a
ashthespy <ashthespy@gmail.com>
2020-03-09 13:42:48 +0100

    Ensure sink restarts b/w tracks when gapless is disabled

--- playback/src/player.rs
+++ playback/src/player.rs
@@ -828,8 +828,7 @@ impl Future for PlayerInternal {
                 ..
             } = self.state
             {
-                if self.config.gapless
-                    && (!*suggested_to_preload_next_track)
+                if (!*suggested_to_preload_next_track)
                     && ((duration_ms as i64 - Self::position_pcm_to_ms(stream_position_pcm) as i64)
                         < PRELOAD_NEXT_TRACK_BEFORE_END_DURATION_MS as i64)
                     && stream_loader_controller.range_to_end_available()
@@ -1066,6 +1065,9 @@ impl PlayerInternal {
         play: bool,
         position_ms: u32,
     ) {
+        if !self.config.gapless {
+            self.ensure_sink_stopped();
+        }
         // emit the correct player event
         match self.state {
             PlayerState::Playing {
