ed0e1eed5a99336b2841006b5a20399676f8bf2b
v1ne <v1ne2go+git@gmail.com>
2020-05-02 12:14:53 +0200

    Synchronise name and help: softvol (#470)
    Using "softmixer" as a mixer backend does not work.
    
    Co-authored-by: v1ne <v1ne2go@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -129,7 +129,7 @@ fn setup(args: &[String]) -> Setup {
             "Audio device to use. Use '?' to list options if using portaudio or alsa",
             "DEVICE",
         )
-        .optopt("", "mixer", "Mixer to use (alsa or softmixer)", "MIXER")
+        .optopt("", "mixer", "Mixer to use (alsa or softvol)", "MIXER")
         .optopt(
             "m",
             "mixer-name",
