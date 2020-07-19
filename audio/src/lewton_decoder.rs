5e28b0e8a65b85341ed31aa1306cbee248ca6971
William R. Fraser <wfraser@codewise.org>
2020-07-18 18:01:24 -0700

    two missed hidden lifetimes

--- audio/src/lewton_decoder.rs
+++ audio/src/lewton_decoder.rs
@@ -57,13 +57,13 @@ impl From<lewton::VorbisError> for VorbisError {
 }
 
 impl fmt::Debug for VorbisError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         fmt::Debug::fmt(&self.0, f)
     }
 }
 
 impl fmt::Display for VorbisError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         fmt::Display::fmt(&self.0, f)
     }
 }
