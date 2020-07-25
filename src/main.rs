09bebe5dd7ad1f637f9767a2e12d838949afc3d7
Laurent Louf <laurentlouf@gmail.com>
2020-07-25 13:51:45 +0200

    Fix types

--- src/main.rs
+++ src/main.rs
@@ -250,8 +250,8 @@ fn setup(args: &[String]) -> Setup {
     let use_audio_cache = !matches.opt_present("disable-audio-cache");
 
     let cache_directory = matches.opt_str("c").unwrap_or(String::from(""));
-    let system_cache_directory = matches.opt_str("system-cache").unwrap_or(String::from(cache_directory))
-    let cache = Cache::new(
+    let system_cache_directory = matches.opt_str("system-cache").unwrap_or(String::from(cache_directory.clone()));
+    let cache = Some(Cache::new(
         PathBuf::from(cache_directory),
         PathBuf::from(system_cache_directory),
         use_audio_cache));
