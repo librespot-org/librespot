ec1ec59b8a33d24d69018350da0851ef310e2732
ThouCheese <luuk.wester@gmail.com>
2021-03-06 01:29:08 +0100

    update examples
    Re-add default impl to SessionConfig and make Credentials::with_password generic over Into<String>
    
    add docs for Credential
    
    reintroduce old Default impl for SessionConfig
    
    use the third argument for the track-to-play rather than a testing id

--- examples/get_token.rs
+++ examples/get_token.rs
@@ -1,5 +1,4 @@
 use std::env;
-use tokio_core::reactor::Core;
 
 use librespot::core::authentication::Credentials;
 use librespot::core::config::SessionConfig;
@@ -9,29 +8,24 @@ use librespot::core::session::Session;
 const SCOPES: &str =
     "streaming,user-read-playback-state,user-modify-playback-state,user-read-currently-playing";
 
-fn main() {
-    let mut core = Core::new().unwrap();
-    let handle = core.handle();
-
+#[tokio::main]
+async fn main() {
     let session_config = SessionConfig::default();
 
     let args: Vec<_> = env::args().collect();
     if args.len() != 4 {
-        println!("Usage: {} USERNAME PASSWORD CLIENT_ID", args[0]);
+        eprintln!("Usage: {} USERNAME PASSWORD CLIENT_ID", args[0]);
+        return;
     }
-    let username = args[1].to_owned();
-    let password = args[2].to_owned();
-    let client_id = &args[3];
 
     println!("Connecting..");
-    let credentials = Credentials::with_password(username, password);
-    let session = core
-        .run(Session::connect(session_config, credentials, None, handle))
-        .unwrap();
+    let credentials = Credentials::with_password(&args[1], &args[2]);
+    let session = Session::connect(session_config, credentials, None).await.unwrap();
 
     println!(
         "Token: {:#?}",
-        core.run(keymaster::get_token(&session, &client_id, SCOPES))
+        keymaster::get_token(&session, &args[3], SCOPES)
+            .await
             .unwrap()
     );
 }
--- examples/play.rs
+++ examples/play.rs
@@ -1,47 +1,41 @@
 use std::env;
-use tokio_core::reactor::Core;
 
 use librespot::core::authentication::Credentials;
 use librespot::core::config::SessionConfig;
 use librespot::core::session::Session;
 use librespot::core::spotify_id::SpotifyId;
 use librespot::playback::config::PlayerConfig;
-
 use librespot::playback::audio_backend;
 use librespot::playback::player::Player;
 
-fn main() {
-    let mut core = Core::new().unwrap();
-    let handle = core.handle();
-
+#[tokio::main]
+async fn main() {
     let session_config = SessionConfig::default();
     let player_config = PlayerConfig::default();
 
     let args: Vec<_> = env::args().collect();
     if args.len() != 4 {
-        println!("Usage: {} USERNAME PASSWORD TRACK", args[0]);
+        eprintln!("Usage: {} USERNAME PASSWORD TRACK", args[0]);
+        return;
     }
-    let username = args[1].to_owned();
-    let password = args[2].to_owned();
-    let credentials = Credentials::with_password(username, password);
+    let credentials = Credentials::with_password(&args[1], &args[2]);
 
     let track = SpotifyId::from_base62(&args[3]).unwrap();
 
     let backend = audio_backend::find(None).unwrap();
 
     println!("Connecting ..");
-    let session = core
-        .run(Session::connect(session_config, credentials, None, handle))
-        .unwrap();
+    let session = Session::connect(session_config, credentials, None).await.unwrap();
 
-    let (mut player, _) = Player::new(player_config, session.clone(), None, move || {
-        (backend)(None)
+    let (mut player, _) = Player::new(player_config, session, None, move || {
+        backend(None)
     });
 
     player.load(track, true, 0);
 
     println!("Playing...");
-    core.run(player.get_end_of_track_future()).unwrap();
+
+    player.await_end_of_track().await;
 
     println!("Done");
 }
