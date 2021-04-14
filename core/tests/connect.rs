68818758a217d078baabe9d854b65b40527a64f0
johannesd3 <johannesd@outlook.com>
2021-04-14 19:32:15 +0200

    Add timeout to test

--- core/tests/connect.rs
+++ core/tests/connect.rs
@@ -1,18 +1,26 @@
+use std::time::Duration;
+
 use librespot_core::authentication::Credentials;
 use librespot_core::config::SessionConfig;
 use librespot_core::session::Session;
 
+use tokio::time::timeout;
+
 #[tokio::test]
 async fn test_connection() {
-    let result = Session::connect(
-        SessionConfig::default(),
-        Credentials::with_password("test", "test"),
-        None,
-    )
-    .await;
+    timeout(Duration::from_secs(30), async {
+        let result = Session::connect(
+            SessionConfig::default(),
+            Credentials::with_password("test", "test"),
+            None,
+        )
+        .await;
 
-    match result {
-        Ok(_) => panic!("Authentication succeeded despite of bad credentials."),
-        Err(e) => assert_eq!(e.to_string(), "Login failed with reason: Bad credentials"),
-    };
+        match result {
+            Ok(_) => panic!("Authentication succeeded despite of bad credentials."),
+            Err(e) => assert_eq!(e.to_string(), "Login failed with reason: Bad credentials"),
+        }
+    })
+    .await
+    .unwrap();
 }
