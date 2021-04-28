2ab413682fda1fbf6fe569fa2f327dbc4392e307
Roderick van Domburg <roderick@vandomburg.net>
2021-04-28 20:51:59 +0200

    Fix high pass ditherer on interleaved samples

--- audio/src/dither.rs
+++ audio/src/dither.rs
@@ -2,6 +2,8 @@ use rand::rngs::ThreadRng;
 use rand_distr::{Distribution, Normal, Triangular, Uniform};
 use std::fmt;
 
+const NUM_CHANNELS: usize = 2;
+
 // Dithering lowers digital-to-analog conversion ("requantization") error,
 // lowering distortion and replacing it with a constant, fixed noise level,
 // which is more pleasant to the ear than the distortion. Doing so can with
@@ -179,7 +181,8 @@ impl Ditherer for GaussianDitherer {
 // filter with weights [1.0, -1.0], and is superseded by noise shapers.
 // Guidance: better than Triangular if not doing other noise shaping.
 pub struct HighPassDitherer {
-    previous_noise: f32,
+    active_channel: usize,
+    previous_noises: [f32; NUM_CHANNELS],
     cached_rng: ThreadRng,
     distribution: Uniform<f32>,
 }
@@ -187,7 +190,8 @@ pub struct HighPassDitherer {
 impl Ditherer for HighPassDitherer {
     fn new() -> Self {
         Self {
-            previous_noise: 0.0,
+            active_channel: 0,
+            previous_noises: [0.0; NUM_CHANNELS],
             cached_rng: rand::thread_rng(),
             distribution: Uniform::new_inclusive(-0.5, 0.5), // 1 LSB +/- 1 LSB (previous) = 2 LSB
         }
@@ -199,8 +203,9 @@ impl Ditherer for HighPassDitherer {
 
     fn noise(&mut self, _sample: f32) -> f32 {
         let new_noise = self.distribution.sample(&mut self.cached_rng);
-        let high_passed_noise = new_noise - self.previous_noise;
-        self.previous_noise = new_noise;
+        let high_passed_noise = new_noise - self.previous_noises[self.active_channel];
+        self.previous_noises[self.active_channel] = new_noise;
+        self.active_channel ^= 1;
         high_passed_noise
     }
 }
