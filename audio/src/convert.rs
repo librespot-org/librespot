3c527e4db33bd0f1718e7baa83c924c2a695975a
Roderick van Domburg <roderick@vandomburg.net>
2021-04-30 23:14:38 +0200

    Refactor sample conversion into `Requantizer` struct
     * Arguable nicer API with smaller code size
    
     * Slightly improved performance for S24_3

--- audio/src/convert.rs
+++ audio/src/convert.rs
@@ -34,72 +34,76 @@ impl Requantizer {
         }
     }
 
-    pub fn shaped_dither(&mut self, sample: f32) -> f32 {
+    // Denormalize, dither and shape noise
+    pub fn scale(&mut self, sample: f32, max: i32) -> f32 {
+        // Losslessly represent [-1.0..1.0] as some signed integer value while
+        // maintaining DC linearity. There is nothing to be gained by doing
+        // this in f64, as the significand of a f32 is 24 bits, just like the
+        // maximum bit depth we are converting to. Taken from:
+        // http://blog.bjornroche.com/2009/12/int-float-int-its-jungle-out-there.html
+        let int_value = sample * (max as f32 + 0.5) - 0.5;
+        self.shaped_dither(int_value)
+    }
+
+    // Special case for samples packed in a word of greater bit depth (e.g.
+    // S24): clamp between min and max to ensure that the most significant
+    // byte is zero. Otherwise, dithering may cause an overflow. This is not
+    // necessary for other formats, because casting to integer will saturate
+    // to the bounds of the primitive.
+    pub fn clamping_scale(&mut self, sample: f32, min: i32, max: i32) -> f32 {
+        let int_value = self.scale(sample, max);
+
+        let min = min as f32;
+        let max = max as f32;
+        if int_value < min {
+            return min;
+        } else if int_value > max {
+            return max;
+        }
+        int_value
+    }
+
+    fn shaped_dither(&mut self, sample: f32) -> f32 {
         let noise = self.ditherer.noise(sample);
         self.noise_shaper.shape(sample, noise)
     }
 }
 
-macro_rules! convert_samples_to {
-    ($type: ident, $samples: expr, $requantizer: expr) => {
-        convert_samples_to!($type, $samples, $requantizer, $type)
-    };
-    ($type: ident, $samples: expr, $requantizer: expr, $scale: ident) => {
-        $samples
-            .iter()
-            .map(|sample| {
-                // Losslessly represent [-1.0, 1.0] to [$type::MIN, $type::MAX]
-                // while maintaining DC linearity. There is nothing to be gained
-                // by doing this in f64, as the significand of a f32 is 24 bits,
-                // just like the maximum bit depth we are converting to.
-                let mut int_value = *sample * ($scale::MAX as f32 + 0.5) - 0.5;
-                int_value = $requantizer.shaped_dither(int_value);
-
-                // Special case for output formats with a range smaller than
-                // their primitive allows (i.e. S24: 24-bit samples in a 32-bit
-                // word): clamp between MIN and MAX to ensure that the most
-                // significant byte is zero. Otherwise, dithering may cause an
-                // overflow. Don't care in other cases as casting to primitives
-                // will saturate correctly (see below).
-                if ($scale::MAX as $type) < std::$type::MAX {
-                    let min = $scale::MIN as f32;
-                    let max = $scale::MAX as f32;
-
-                    if int_value < min {
-                        int_value = min;
-                    } else if int_value > max {
-                        int_value = max;
-                    }
-                }
-
-                // https://doc.rust-lang.org/nomicon/casts.html:
-                // casting float to integer rounds towards zero, then saturates.
-                // Ideally halves should round to even to prevent any bias, but
-                // since it is extremely unlikely that a float has *exactly* .5
-                // as fraction, this should be more than precise enough.
-                int_value as $type
-            })
-            .collect()
-    };
-}
-
+// https://doc.rust-lang.org/nomicon/casts.html: casting float to integer
+// rounds towards zero, then saturates. Ideally halves should round to even to
+// prevent any bias, but since it is extremely unlikely that a float has
+// *exactly* .5 as fraction, this should be more than precise enough.
 pub fn to_s32(samples: &[f32], requantizer: &mut Requantizer) -> Vec<i32> {
-    convert_samples_to!(i32, samples, requantizer)
+    samples
+        .iter()
+        .map(|sample| requantizer.scale(*sample, std::i32::MAX) as i32)
+        .collect()
 }
 
 // S24 is 24-bit PCM packed in an upper 32-bit word
 pub fn to_s24(samples: &[f32], requantizer: &mut Requantizer) -> Vec<i32> {
-    convert_samples_to!(i32, samples, requantizer, i24)
+    samples
+        .iter()
+        .map(|sample| requantizer.clamping_scale(*sample, i24::MIN, i24::MAX) as i32)
+        .collect()
 }
 
+// S24_3 is 24-bit PCM in a 3-byte array
 pub fn to_s24_3(samples: &[f32], requantizer: &mut Requantizer) -> Vec<i24> {
-    // TODO - can we improve performance by passing this as a closure?
-    to_s24(samples, requantizer)
+    samples
         .iter()
-        .map(|sample| i24::from_s24(*sample))
+        .map(|sample| {
+            // Not as DRY as calling to_s24 first, but this saves iterating
+            // over all samples twice.
+            let int_value = requantizer.clamping_scale(*sample, i24::MIN, i24::MAX) as i32;
+            i24::from_s24(int_value)
+        })
         .collect()
 }
 
 pub fn to_s16(samples: &[f32], requantizer: &mut Requantizer) -> Vec<i16> {
-    convert_samples_to!(i16, samples, requantizer)
+    samples
+        .iter()
+        .map(|sample| requantizer.scale(*sample, std::i16::MAX as i32) as i16)
+        .collect()
 }
