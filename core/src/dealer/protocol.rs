7ed35396f85e16dbe939841c05edd37ec05f863e
Johannesd3 <51954457+Johannesd3@users.noreply.github.com>
2021-05-27 15:33:29 +0200

    Mostly cosmetic changes in `dealer` (#762)
    * Add missing timeout on reconnect
    
    * Cosmetic changes

--- core/src/dealer/protocol.rs
+++ core/src/dealer/protocol.rs
@@ -6,15 +6,6 @@ pub type JsonValue = serde_json::Value;
 pub type JsonObject = serde_json::Map<String, JsonValue>;
 
 #[derive(Clone, Debug, Deserialize)]
-pub struct Request<P> {
-    #[serde(default)]
-    pub headers: HashMap<String, String>,
-    pub message_ident: String,
-    pub key: String,
-    pub payload: P,
-}
-
-#[derive(Clone, Debug, Deserialize)]
 pub struct Payload {
     pub message_id: i32,
     pub sent_by_device_id: String,
@@ -22,18 +13,27 @@ pub struct Payload {
 }
 
 #[derive(Clone, Debug, Deserialize)]
-pub struct Message<P> {
+pub struct Request {
+    #[serde(default)]
+    pub headers: HashMap<String, String>,
+    pub message_ident: String,
+    pub key: String,
+    pub payload: Payload,
+}
+
+#[derive(Clone, Debug, Deserialize)]
+pub struct Message {
     #[serde(default)]
     pub headers: HashMap<String, String>,
     pub method: Option<String>,
     #[serde(default)]
-    pub payloads: Vec<P>,
+    pub payloads: Vec<JsonValue>,
     pub uri: String,
 }
 
 #[derive(Clone, Debug, Deserialize)]
 #[serde(tag = "type", rename_all = "snake_case")]
-pub enum MessageOrRequest {
-    Message(Message<JsonValue>),
-    Request(Request<Payload>),
+pub(super) enum MessageOrRequest {
+    Message(Message),
+    Request(Request),
 }
