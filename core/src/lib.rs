7c975f8ed0e27d0b034997cf7bacd2cd35c9c58e
Roderick van Domburg <roderick@vandomburg.net>
2021-06-01 20:33:10 +0200

    Remove feature gate around `apresolve`

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -8,6 +8,7 @@ use librespot_protocol as protocol;
 #[macro_use]
 mod component;
 
+mod apresolve;
 pub mod audio_key;
 pub mod authentication;
 pub mod cache;
@@ -24,15 +25,3 @@ pub mod spotify_id;
 #[doc(hidden)]
 pub mod util;
 pub mod version;
-
-const AP_FALLBACK: &str = "ap.spotify.com:443";
-
-#[cfg(feature = "apresolve")]
-mod apresolve;
-
-#[cfg(not(feature = "apresolve"))]
-mod apresolve {
-    pub async fn apresolve(_: Option<&url::Url>, _: Option<u16>) -> String {
-        return super::AP_FALLBACK.into();
-    }
-}
