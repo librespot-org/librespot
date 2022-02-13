ab562cc8d873fb9704782b337471ac5544fe7627
Sebastian Dröge <slomo@coaxion.net>
2022-02-13 22:52:02 +0200

    Update GStreamer to 0.18 and clean up (#964)
    * Update GStreamer backend to 0.18
    * Don't manually go through all intermediate states when shutting down the GStreamer backend; that happens automatically
    * Don't initialize GStreamer twice
    * Use less stringly-typed API for configuring the appsrc
    * Create our own main context instead of stealing the default one; if the application somewhere else uses the default main context this would otherwise fail in interesting ways
    * Create GStreamer pipeline more explicitly instead of going via strings for everything
    * Add an audioresample element before the sink in case the sink doesn't support the sample rate
    * Remove unnecessary `as_bytes()` call
    * Use a GStreamer bus sync handler instead of spawning a new thread with a mainloop; it's only used for printing errors or when the end of the stream is reached, which can also be done as well when synchronously handling messages.
    * Change `expect()` calls to proper error returns wherever possible in GStreamer backend
    * Store asynchronously reported error in GStreamer backend and return them on next write
    * Update MSRV to 1.56

--- playback/src/audio_backend/gstreamer.rs
+++ playback/src/audio_backend/gstreamer.rs
@@ -1,14 +1,11 @@
-use std::{ops::Drop, thread};
-
-use gstreamer as gst;
-use gstreamer_app as gst_app;
-
 use gst::{
     event::{FlushStart, FlushStop},
     prelude::*,
     State,
 };
-use zerocopy::AsBytes;
+
+use parking_lot::Mutex;
+use std::sync::Arc;
 
 use super::{Open, Sink, SinkAsBytes, SinkError, SinkResult};
 
@@ -16,12 +13,12 @@ use crate::{
     config::AudioFormat, convert::Converter, decoder::AudioPacket, NUM_CHANNELS, SAMPLE_RATE,
 };
 
-#[allow(dead_code)]
 pub struct GstreamerSink {
     appsrc: gst_app::AppSrc,
     bufferpool: gst::BufferPool,
     pipeline: gst::Pipeline,
     format: AudioFormat,
+    async_error: Arc<Mutex<Option<String>>>,
 }
 
 impl Open for GstreamerSink {
@@ -29,80 +26,95 @@ impl Open for GstreamerSink {
         info!("Using GStreamer sink with format: {:?}", format);
         gst::init().expect("failed to init GStreamer!");
 
-        // GStreamer calls S24 and S24_3 different from the rest of the world
         let gst_format = match format {
-            AudioFormat::S24 => "S24_32".to_string(),
-            AudioFormat::S24_3 => "S24".to_string(),
-            _ => format!("{:?}", format),
+            AudioFormat::F64 => gst_audio::AUDIO_FORMAT_F64,
+            AudioFormat::F32 => gst_audio::AUDIO_FORMAT_F32,
+            AudioFormat::S32 => gst_audio::AUDIO_FORMAT_S32,
+            AudioFormat::S24 => gst_audio::AUDIO_FORMAT_S2432,
+            AudioFormat::S24_3 => gst_audio::AUDIO_FORMAT_S24,
+            AudioFormat::S16 => gst_audio::AUDIO_FORMAT_S16,
         };
+
+        let gst_info = gst_audio::AudioInfo::builder(gst_format, SAMPLE_RATE, NUM_CHANNELS as u32)
+            .build()
+            .expect("Failed to create GStreamer audio format");
+        let gst_caps = gst_info.to_caps().expect("Failed to create GStreamer caps");
+
         let sample_size = format.size();
         let gst_bytes = NUM_CHANNELS as usize * 1024 * sample_size;
 
-        #[cfg(target_endian = "little")]
-        const ENDIANNESS: &str = "LE";
-        #[cfg(target_endian = "big")]
-        const ENDIANNESS: &str = "BE";
-
-        let pipeline_str_preamble = format!(
-            "appsrc caps=\"audio/x-raw,format={}{},layout=interleaved,channels={},rate={}\" block=true max-bytes={} name=appsrc0 ",
-            gst_format, ENDIANNESS, NUM_CHANNELS, SAMPLE_RATE, gst_bytes
-        );
-        // no need to dither twice; use librespot dithering instead
-        let pipeline_str_rest = r#" ! audioconvert dithering=none ! autoaudiosink"#;
-        let pipeline_str: String = match device {
-            Some(x) => format!("{}{}", pipeline_str_preamble, x),
-            None => format!("{}{}", pipeline_str_preamble, pipeline_str_rest),
-        };
-        info!("Pipeline: {}", pipeline_str);
-
-        gst::init().unwrap();
-        let pipelinee = gst::parse_launch(&*pipeline_str).expect("Couldn't launch pipeline; likely a GStreamer issue or an error in the pipeline string you specified in the 'device' argument to librespot.");
-        let pipeline = pipelinee
-            .dynamic_cast::<gst::Pipeline>()
-            .expect("couldn't cast pipeline element at runtime!");
-        let bus = pipeline.bus().expect("couldn't get bus from pipeline");
-        let mainloop = glib::MainLoop::new(None, false);
-        let appsrce: gst::Element = pipeline
-            .by_name("appsrc0")
-            .expect("couldn't get appsrc from pipeline");
-        let appsrc: gst_app::AppSrc = appsrce
-            .dynamic_cast::<gst_app::AppSrc>()
+        let pipeline = gst::Pipeline::new(None);
+        let appsrc = gst::ElementFactory::make("appsrc", None)
+            .expect("Failed to create GStreamer appsrc element")
+            .downcast::<gst_app::AppSrc>()
             .expect("couldn't cast AppSrc element at runtime!");
-        let appsrc_caps = appsrc.caps().expect("couldn't get appsrc caps");
+        appsrc.set_caps(Some(&gst_caps));
+        appsrc.set_max_bytes(gst_bytes as u64);
+        appsrc.set_block(true);
+
+        let sink = match device {
+            None => {
+                // no need to dither twice; use librespot dithering instead
+                gst::parse_bin_from_description(
+                    "audioconvert dithering=none ! audioresample ! autoaudiosink",
+                    true,
+                )
+                .expect("Failed to create default GStreamer sink")
+            }
+            Some(ref x) => gst::parse_bin_from_description(x, true)
+                .expect("Failed to create custom GStreamer sink"),
+        };
+        pipeline
+            .add(&appsrc)
+            .expect("Failed to add GStreamer appsrc to pipeline");
+        pipeline
+            .add(&sink)
+            .expect("Failed to add GStreamer sink to pipeline");
+        appsrc
+            .link(&sink)
+            .expect("Failed to link GStreamer source to sink");
+
+        let bus = pipeline.bus().expect("couldn't get bus from pipeline");
 
         let bufferpool = gst::BufferPool::new();
 
         let mut conf = bufferpool.config();
-        conf.set_params(Some(&appsrc_caps), gst_bytes as u32, 0, 0);
+        conf.set_params(Some(&gst_caps), gst_bytes as u32, 0, 0);
         bufferpool
             .set_config(conf)
             .expect("couldn't configure the buffer pool");
 
-        thread::spawn(move || {
-            let thread_mainloop = mainloop;
-            let watch_mainloop = thread_mainloop.clone();
-            bus.add_watch(move |_, msg| {
-                match msg.view() {
-                    gst::MessageView::Eos(_) => {
-                        println!("gst signaled end of stream");
-                        watch_mainloop.quit();
-                    }
-                    gst::MessageView::Error(err) => {
-                        println!(
-                            "Error from {:?}: {} ({:?})",
-                            err.src().map(|s| s.path_string()),
-                            err.error(),
-                            err.debug()
-                        );
-                        watch_mainloop.quit();
-                    }
-                    _ => (),
-                };
+        let async_error = Arc::new(Mutex::new(None));
+        let async_error_clone = async_error.clone();
 
-                glib::Continue(true)
-            })
-            .expect("failed to add bus watch");
-            thread_mainloop.run();
+        bus.set_sync_handler(move |_bus, msg| {
+            match msg.view() {
+                gst::MessageView::Eos(_) => {
+                    println!("gst signaled end of stream");
+
+                    let mut async_error_storage = async_error_clone.lock();
+                    *async_error_storage = Some(String::from("gst signaled end of stream"));
+                }
+                gst::MessageView::Error(err) => {
+                    println!(
+                        "Error from {:?}: {} ({:?})",
+                        err.src().map(|s| s.path_string()),
+                        err.error(),
+                        err.debug()
+                    );
+
+                    let mut async_error_storage = async_error_clone.lock();
+                    *async_error_storage = Some(format!(
+                        "Error from {:?}: {} ({:?})",
+                        err.src().map(|s| s.path_string()),
+                        err.error(),
+                        err.debug()
+                    ));
+                }
+                _ => (),
+            }
+
+            gst::BusSyncReply::Drop
         });
 
         pipeline
@@ -114,16 +126,18 @@ impl Open for GstreamerSink {
             bufferpool,
             pipeline,
             format,
+            async_error,
         }
     }
 }
 
 impl Sink for GstreamerSink {
     fn start(&mut self) -> SinkResult<()> {
+        *self.async_error.lock() = None;
         self.appsrc.send_event(FlushStop::new(true));
         self.bufferpool
             .set_active(true)
-            .expect("couldn't activate buffer pool");
+            .map_err(|e| SinkError::StateChange(e.to_string()))?;
         self.pipeline
             .set_state(State::Playing)
             .map_err(|e| SinkError::StateChange(e.to_string()))?;
@@ -131,13 +145,14 @@ impl Sink for GstreamerSink {
     }
 
     fn stop(&mut self) -> SinkResult<()> {
+        *self.async_error.lock() = None;
         self.appsrc.send_event(FlushStart::new());
         self.pipeline
             .set_state(State::Paused)
             .map_err(|e| SinkError::StateChange(e.to_string()))?;
         self.bufferpool
             .set_active(false)
-            .expect("couldn't deactivate buffer pool");
+            .map_err(|e| SinkError::StateChange(e.to_string()))?;
         Ok(())
     }
 
@@ -146,15 +161,16 @@ impl Sink for GstreamerSink {
 
 impl Drop for GstreamerSink {
     fn drop(&mut self) {
-        // Follow the state transitions documented at:
-        // https://gstreamer.freedesktop.org/documentation/additional/design/states.html?gi-language=c
-        let _ = self.pipeline.set_state(State::Ready);
         let _ = self.pipeline.set_state(State::Null);
     }
 }
 
 impl SinkAsBytes for GstreamerSink {
     fn write_bytes(&mut self, data: &[u8]) -> SinkResult<()> {
+        if let Some(async_error) = &*self.async_error.lock() {
+            return Err(SinkError::OnWrite(async_error.to_string()));
+        }
+
         let mut buffer = self
             .bufferpool
             .acquire_buffer(None)
@@ -163,8 +179,8 @@ impl SinkAsBytes for GstreamerSink {
         let mutbuf = buffer.make_mut();
         mutbuf.set_size(data.len());
         mutbuf
-            .copy_from_slice(0, data.as_bytes())
-            .expect("Failed to copy from slice");
+            .copy_from_slice(0, data)
+            .map_err(|e| SinkError::OnWrite(e.to_string()))?;
 
         self.appsrc
             .push_buffer(buffer)
