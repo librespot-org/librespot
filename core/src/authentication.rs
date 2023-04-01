cd1f16e4e675a682d0aa41b54e2221ba1cbd7502
Christian König <ckoenig@posteo.de>
2023-04-01 15:23:07 +0200

    Handle result
    Signed-off-by: Christian König <ckoenig@posteo.de>

--- core/src/authentication.rs
+++ core/src/authentication.rs
@@ -96,7 +96,9 @@ impl Credentials {
                 return Err(AuthenticationError::Key.into());
             }
 
-            pbkdf2::<Hmac<Sha1>>(&secret, username.as_bytes(), 0x100, &mut key[0..20]);
+            let res = pbkdf2::<Hmac<Sha1>>(&secret, username.as_bytes(), 0x100, &mut key[0..20]);
+            assert!(res.is_ok(),"Failed to create hash");
+
 
             let hash = &Sha1::digest(&key[..20]);
             key[..20].copy_from_slice(hash);
