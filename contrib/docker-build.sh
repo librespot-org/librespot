4f8a84e7cb3f257bb4ca185e10ec9bd3cc7b1bbf
Dariusz Olszewski <starypatyk@users.noreply.github.com>
2024-06-11 20:55:27 +0200

    Docker cross compile - remove MIPS target platform
    Signed-off-by: Dariusz Olszewski <starypatyk@users.noreply.github.com>

--- contrib/docker-build.sh
+++ contrib/docker-build.sh
@@ -5,4 +5,3 @@ cargo build --release --no-default-features --features alsa-backend
 cargo build --release --target aarch64-unknown-linux-gnu --no-default-features --features alsa-backend
 cargo build --release --target arm-unknown-linux-gnueabihf --no-default-features --features alsa-backend
 cargo build --release --target arm-unknown-linux-gnueabi --no-default-features --features alsa-backend
-cargo build --release --target mipsel-unknown-linux-gnu --no-default-features --features alsa-backend
