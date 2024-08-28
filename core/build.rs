dc229106735aba77bbcb2f72d6079ade9f44db5b
yubiuser <github@yubiuser.dev>
2024-08-28 13:13:29 +0200

    Update vergen to version 9 by using the new vergen-gitctl libary
    Signed-off-by: yubiuser <github@yubiuser.dev>

--- core/build.rs
+++ core/build.rs
@@ -1,11 +1,19 @@
 use rand::{distributions::Alphanumeric, Rng};
-use vergen::EmitBuilder;
+use vergen_gitcl::{BuildBuilder, Emitter, GitclBuilder};
 
-fn main() {
-    EmitBuilder::builder()
-        .build_date() // outputs 'VERGEN_BUILD_DATE'
-        .git_sha(true) // outputs 'VERGEN_GIT_SHA', and sets the 'short' flag true
-        .git_commit_date() // outputs 'VERGEN_GIT_COMMIT_DATE'
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let gitcl = GitclBuilder::default()
+        .sha(true) // outputs 'VERGEN_GIT_SHA', and sets the 'short' flag true
+        .commit_date(true) // outputs 'VERGEN_GIT_COMMIT_DATE'
+        .build()?;
+
+    let build = BuildBuilder::default()
+        .build_date(true) // outputs 'VERGEN_BUILD_DATE'
+        .build()?;
+
+    Emitter::default()
+        .add_instructions(&build)?
+        .add_instructions(&gitcl)?
         .emit()
         .expect("Unable to generate the cargo keys!");
     let build_id = match std::env::var("SOURCE_DATE_EPOCH") {
@@ -18,4 +26,5 @@ fn main() {
     };
 
     println!("cargo:rustc-env=LIBRESPOT_BUILD_ID={build_id}");
+    Ok(())
 }
