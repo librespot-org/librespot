da0f6a10ab9d9e359c8cf3249ef0e6ab2b6e0c90
Nick Steel <nick@nsteel.co.uk>
2024-08-29 02:31:47 +0100

    oauth: provide example

---
+++ oauth/examples/oauth.rs
@@ -0,0 +1,13 @@
+use librespot_core::SessionConfig;
+use librespot_oauth::get_access_token;
+
+fn main() {
+    let mut builder = env_logger::Builder::new();
+    builder.parse_filters("librespot=trace");
+    builder.init();
+    
+    match get_access_token(&SessionConfig::default().client_id,  vec!["streaming"], Some(1337)) {
+        Ok(token) => println!("Success: {token:#?}"),
+        Err(e) => println!("Failed: {e}"),
+    };
+}
