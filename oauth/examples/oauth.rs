68eebc853f1dbc4d4fc06b0d14da05f03d77cda6
Nick Steel <nick@nsteel.co.uk>
2024-09-08 23:55:41 +0100

    oauth: Remove examples's core dependency

--- oauth/examples/oauth.rs
+++ oauth/examples/oauth.rs
@@ -1,16 +1,14 @@
-use librespot_core::SessionConfig;
 use librespot_oauth::get_access_token;
+
+// You can use any client ID here but it must be configured to allow redirect URI http://127.0.0.1
+const SPOTIFY_CLIENT_ID: &str = "65b708073fc0480ea92a077233ca87bd";
 
 fn main() {
     let mut builder = env_logger::Builder::new();
     builder.parse_filters("librespot=trace");
     builder.init();
 
-    match get_access_token(
-        &SessionConfig::default().client_id,
-        vec!["streaming"],
-        Some(1337),
-    ) {
+    match get_access_token(SPOTIFY_CLIENT_ID, vec!["streaming"], Some(1337)) {
         Ok(token) => println!("Success: {token:#?}"),
         Err(e) => println!("Failed: {e}"),
     };
