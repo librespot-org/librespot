0f497cc690e4fefef83686864155d6d68ab2977f
Dariusz Olszewski <starypatyk@users.noreply.github.com>
2024-12-25 22:43:09 +0100

    Cross-compile with libmdns
    Signed-off-by: Dariusz Olszewski <starypatyk@users.noreply.github.com>

--- contrib/cross-compile-armv6hf/docker-build.sh
+++ contrib/cross-compile-armv6hf/docker-build.sh
@@ -11,4 +11,4 @@ PI1_LIB_DIRS=(
 )
 export RUSTFLAGS="-C linker=$PI1_TOOLS_DIR/bin/arm-linux-gnueabihf-gcc ${PI1_LIB_DIRS[*]/#/-L}"
 
-cargo build --release --target arm-unknown-linux-gnueabihf --no-default-features --features "alsa-backend"
+cargo build --release --target arm-unknown-linux-gnueabihf --no-default-features --features "alsa-backend with-libmdns"
--- contrib/docker-build.sh
+++ contrib/docker-build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -eux
 
-cargo build --release --no-default-features --features alsa-backend
-cargo build --release --target aarch64-unknown-linux-gnu --no-default-features --features alsa-backend
-cargo build --release --target arm-unknown-linux-gnueabihf --no-default-features --features alsa-backend
-cargo build --release --target arm-unknown-linux-gnueabi --no-default-features --features alsa-backend
+cargo build --release --no-default-features --features "alsa-backend with-libmdns"
+cargo build --release --target aarch64-unknown-linux-gnu --no-default-features --features "alsa-backend with-libmdns"
+cargo build --release --target arm-unknown-linux-gnueabihf --no-default-features --features "alsa-backend with-libmdns"
+cargo build --release --target arm-unknown-linux-gnueabi --no-default-features --features "alsa-backend with-libmdns"
