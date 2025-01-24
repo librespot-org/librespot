3026d030b552e5fa4cb2c7d8f4981a7d2f069fc7
Dariusz Olszewski <starypatyk@users.noreply.github.com>
2025-01-24 21:18:47 +0100

    Simplify Docker image to cross-compile for armv6hf (RPi 1)
    Signed-off-by: Dariusz Olszewski <starypatyk@users.noreply.github.com>

--- contrib/cross-compile-armv6hf/docker-build.sh
+++ contrib/cross-compile-armv6hf/docker-build.sh
@@ -7,7 +7,6 @@ PI1_LIB_DIRS=(
   "$PI1_TOOLS_DIR/arm-linux-gnueabihf/sysroot/lib"
   "$PI1_TOOLS_DIR/arm-linux-gnueabihf/sysroot/usr/lib"
   "/sysroot/usr/lib/arm-linux-gnueabihf"
-  "/sysroot/lib/arm-linux-gnueabihf"
 )
 export RUSTFLAGS="-C linker=$PI1_TOOLS_DIR/bin/arm-linux-gnueabihf-gcc ${PI1_LIB_DIRS[*]/#/-L}"
 
