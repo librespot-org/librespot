cf61ede6c694556b0587a47e1630e7521bc9d8e9
Dariusz Olszewski <8277636+starypatyk@users.noreply.github.com>
2025-02-03 22:50:47 +0100

    Fix cross compilation for armv6hf (Raspberry Pi 1) (#1457)
    * Use ring instead of aws-lc as the default backend for hyper-rustls
    
    Signed-off-by: Dariusz Olszewski <starypatyk@users.noreply.github.com>
    
    * Cross-compile with libmdns
    
    Signed-off-by: Dariusz Olszewski <starypatyk@users.noreply.github.com>
    
    * Simplify Docker image to cross-compile for armv6hf (RPi 1)
    
    Signed-off-by: Dariusz Olszewski <starypatyk@users.noreply.github.com>
    
    * Revert "Use ring instead of aws-lc as the default backend for hyper-rustls"
    
    This reverts commit faeaf506d64ab988d6cc950b58dbe22c0eed9386.
    
    * Fix bindgen issues (aws-lc-rs) when cross-compiling for armv6hf
    
    Signed-off-by: Dariusz Olszewski <starypatyk@users.noreply.github.com>
    
    * Add git to the Docker image for cross-compiling
    
    Signed-off-by: Dariusz Olszewski <starypatyk@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Dariusz Olszewski <starypatyk@users.noreply.github.com>
    Co-authored-by: Dariusz Olszewski <starypatyk@users.noreply.github.com>

--- contrib/cross-compile-armv6hf/docker-build.sh
+++ contrib/cross-compile-armv6hf/docker-build.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 set -eux
 
-PI1_TOOLS_DIR="/pi/tools/arm-bcm2708/arm-linux-gnueabihf"
+cargo install --force --locked bindgen-cli
+
+PI1_TOOLS_DIR=/pi/tools/arm-bcm2708/arm-linux-gnueabihf
+PI1_TOOLS_SYSROOT_DIR=$PI1_TOOLS_DIR/arm-linux-gnueabihf/sysroot
 
 PI1_LIB_DIRS=(
-  "$PI1_TOOLS_DIR/arm-linux-gnueabihf/sysroot/lib"
-  "$PI1_TOOLS_DIR/arm-linux-gnueabihf/sysroot/usr/lib"
+  "$PI1_TOOLS_SYSROOT_DIR/lib"
+  "$PI1_TOOLS_SYSROOT_DIR/usr/lib"
   "/sysroot/usr/lib/arm-linux-gnueabihf"
-  "/sysroot/lib/arm-linux-gnueabihf"
 )
 export RUSTFLAGS="-C linker=$PI1_TOOLS_DIR/bin/arm-linux-gnueabihf-gcc ${PI1_LIB_DIRS[*]/#/-L}"
+export BINDGEN_EXTRA_CLANG_ARGS=--sysroot=$PI1_TOOLS_SYSROOT_DIR
 
-cargo build --release --target arm-unknown-linux-gnueabihf --no-default-features --features "alsa-backend"
+cargo build --release --target arm-unknown-linux-gnueabihf --no-default-features --features "alsa-backend with-libmdns"
