601bc09bbd3c444377313e7530cb78d98179ac12
Xyntho <krishnab0406@gmail.com>
2025-03-12 18:48:45 +0530

    refactor code, add error handling, add automatic token retrieval through config

--- src/bin/fkspot.rs
+++ src/bin/fkspot.rs
@@ -1,14 +1,23 @@
-// TODO
-// Each time a request is made, compare accessTokenExpirationTimestampMs
-// with current time so we know if token is expired or not
-// if expired, rerun session init
+const PORT: u16 = 3745;
 
-use actix_web::{get, web, App, HttpServer, Responder};
-use librespot::core::{authentication::Credentials, config::SessionConfig, session::Session};
-use librespot_core::{audio_key::AudioKey, FileId, SpotifyId};
-use std::fs;
+mod util;
+use actix_web::error::ErrorBadRequest;
+// Imports
+use actix_web::{get, web, App, HttpServer};
+use librespot::core::{
+    audio_key::AudioKey, authentication::Credentials, config::SessionConfig, session::Session,
+    FileId, SpotifyId,
+};
+use std::{
+    fs,
+    sync::{LazyLock, Mutex},
+    time::{SystemTime, UNIX_EPOCH},
+};
+
+
 
 #[derive(serde::Serialize, serde::Deserialize, Debug)]
+#[allow(non_snake_case)] // this is for json parsing, ignore naming conventions therefore
 struct SpotRes {
     clientId: String,
     accessToken: String,
@@ -37,16 +46,18 @@ fn read_config() -> (String, String, String) {
 
 struct Connection {
     session: Session,
+    access_token_expiration_timestamp_ms: i64,
 }
 
-use std::sync::LazyLock;
-
-static GLOBAL_CONN: LazyLock<Connection> = LazyLock::new(|| Connection {
-    session: Session::new(SessionConfig::default(), None),
+static GLOBAL_CONN: LazyLock<Mutex<Connection>> = LazyLock::new(|| {
+    Mutex::new(Connection {
+        session: Session::new(SessionConfig::default(), None),
+        access_token_expiration_timestamp_ms: 0,
+    })
 });
 
 impl Connection {
-    pub async fn init(&self) {
+    pub async fn init(&mut self) {
         let access_token = self.get_token().await;
         println!("access_token: {}", access_token);
         let credentials = Credentials::with_access_token(access_token);
@@ -57,7 +68,7 @@ impl Connection {
             .expect("failed to connect with credentials");
     }
 
-    async fn get_token(&self) -> String {
+    async fn get_token(&mut self) -> String {
         let (sp_t, sp_dc, sp_key) = read_config();
         let client = reqwest::Client::new();
         let res = client
@@ -74,40 +85,81 @@ impl Connection {
             .json::<SpotRes>()
             .await
             .unwrap();
-        
+
         if res.isAnonymous {
             panic!("Invalid credentials")
         }
-        
+
+        self.access_token_expiration_timestamp_ms = res.accessTokenExpirationTimestampMs;
+
         res.accessToken
     }
 
-    pub async fn get_audio_key(&self, track_id: &str, file_id: &str) -> AudioKey {
-        let spot_id: SpotifyId =
-            SpotifyId::from_uri(format!("spotify:track:{}", track_id).as_str())
-                .expect("failed to create spotify id object");
+    pub async fn get_audio_key(
+        &mut self,
+        track_id: &str,
+        file_id: &str,
+    ) -> Result<AudioKey, Box<dyn std::error::Error>> {
+        // compare current time to expiration time
+        let current_time = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .expect("Time went backwards")
+            .as_millis() as i64;
 
-        self.session
+        if current_time >= self.access_token_expiration_timestamp_ms {
+            self.session.shutdown();
+            self.init().await;
+        }
+
+        println!(
+            "{}, {}",
+            current_time, self.access_token_expiration_timestamp_ms
+        );
+        let spot_id: SpotifyId =
+            match SpotifyId::from_uri(format!("spotify:track:{}", track_id).as_str()) {
+                Ok(id) => id,
+                Err(e) => return Err(Box::new(e)),
+            };
+
+        let aud_key: AudioKey = self
+            .session
             .audio_key()
             .request(spot_id, FileId::from_raw(file_id.as_bytes()))
-            .await
-            .expect("failed to request audio key")
+            .await?;
+
+        Ok(aud_key)
     }
 }
 
-#[get("/audiokey/{track}")]
-async fn audio_key(track: web::Path<String>) -> impl Responder {
-    let spl = track.split("*");
+// Web server implementation starts here
+
+#[get("/audiokey/{track_plus_file}")]
+async fn audio_key(
+    track_plus_file: web::Path<String>,
+) -> Result<actix_web::web::Bytes, actix_web::Error> {
+    // split the track plus file by an asterik (*)
+    let spl: std::str::Split<'_, &str> = track_plus_file.split("*");
     let collection: Vec<&str> = spl.collect();
-    let key: AudioKey = GLOBAL_CONN
+
+    let key: AudioKey = match GLOBAL_CONN
+        .lock()
+        .unwrap()
         .get_audio_key(collection[0], collection[1])
-        .await;
-    web::Bytes::copy_from_slice(&key.0)
+        .await
+    {
+        Ok(key) => key,
+        Err(_e) => return Err(ErrorBadRequest("Bad Request")),
+    };
+
+    // pass the audio key that was retrieved as raw bytes
+    Ok(web::Bytes::copy_from_slice(&key.0))
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    GLOBAL_CONN.init().await;
+    // intialize on start (not necessary considering we will be intializing
+    // even when we don't have a timestamp set and you request a key)
+    GLOBAL_CONN.lock().unwrap().init().await;
 
     // Example usage:
     // let track_id = "5B5M9o7xEcq6FdEeXrByY0";
@@ -115,8 +167,9 @@ async fn main() -> std::io::Result<()> {
     // let key: AudioKey = GLOBAL_CONN.get_audio_key(track_id, file_id).await;
     // println!("key: {:?}", key);
 
+    println!("Starting web server on http://localhost:{}", PORT);
     HttpServer::new(|| App::new().service(audio_key))
-        .bind(("127.0.0.1", 3745))?
+        .bind(("127.0.0.1", PORT))?
         .run()
         .await
 }
