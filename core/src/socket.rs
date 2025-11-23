6d03c38d2496126a17c3199d3747bf5043427fcd
eladyn <eladyn@protonmail.com>
2025-11-23 15:00:04 +0100

    fix: try all resolved socket addrs for connection
    For the dealer connection, only the first address was tried. For example
    when using a VPN, which doesn't support IPv6, the first address might
    still be an IPv6 address and thus fail. The IPv4 address later in the
    list is never tried. This relies on the tokio default to try all resolved
    addresses and use the first that was successful.

--- core/src/socket.rs
+++ core/src/socket.rs
@@ -1,4 +1,4 @@
-use std::{io, net::ToSocketAddrs};
+use std::io;
 
 use tokio::net::TcpStream;
 use url::Url;
@@ -6,29 +6,14 @@ use url::Url;
 use crate::proxytunnel;
 
 pub async fn connect(host: &str, port: u16, proxy: Option<&Url>) -> io::Result<TcpStream> {
-    let socket = if let Some(proxy_url) = proxy {
+    if let Some(proxy_url) = proxy {
         info!("Using proxy \"{proxy_url}\"");
 
-        let socket_addr = proxy_url.socket_addrs(|| None).and_then(|addrs| {
-            addrs.into_iter().next().ok_or_else(|| {
-                io::Error::new(
-                    io::ErrorKind::NotFound,
-                    "Can't resolve proxy server address",
-                )
-            })
-        })?;
-        let socket = TcpStream::connect(&socket_addr).await?;
+        let socket_addrs = proxy_url.socket_addrs(|| None)?;
+        let socket = TcpStream::connect(&*socket_addrs).await?;
 
-        proxytunnel::proxy_connect(socket, host, &port.to_string()).await?
+        proxytunnel::proxy_connect(socket, host, &port.to_string()).await
     } else {
-        let socket_addr = (host, port).to_socket_addrs()?.next().ok_or_else(|| {
-            io::Error::new(
-                io::ErrorKind::NotFound,
-                "Can't resolve access point address",
-            )
-        })?;
-
-        TcpStream::connect(&socket_addr).await?
-    };
-    Ok(socket)
+        TcpStream::connect((host, port)).await
+    }
 }
