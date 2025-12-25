7b9b3455a1c978def3ccfc2f77752f1447a8f62b
Felix Prillwitz <photovoltex@gmail.com>
2025-12-25 17:59:44 +0100

    fix: add fallback for vergen-gitcl values

--- core/build.rs
+++ core/build.rs
@@ -8,23 +8,34 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .commit_date(true) // outputs 'VERGEN_GIT_COMMIT_DATE'
         .build()?;
 
+    if let Err(why) = Emitter::default()
+        .fail_on_error()
+        .add_instructions(&gitcl)
+        .and_then(|emitter| emitter.emit())
+    {
+        // if we are running into an error here, the git repo is most likely missing
+        // in that case we are probably a dependency and want to emit default values
+        println!("cargo:warning=emitting vergen-gitcl default values: {why}");
+        println!("cargo:rustc-env=VERGEN_GIT_SHA=stable");
+        println!("cargo:rustc-env=VERGEN_GIT_COMMIT_DATE=unknown");
+    }
+
     let build = BuildBuilder::default()
         .build_date(true) // outputs 'VERGEN_BUILD_DATE'
         .build()?;
 
     Emitter::default()
         .add_instructions(&build)?
-        .add_instructions(&gitcl)?
         .emit()
         .expect("Unable to generate the cargo keys!");
-    let build_id = match std::env::var("SOURCE_DATE_EPOCH") {
-        Ok(val) => val,
-        Err(_) => rand::rng()
+
+    let build_id = std::env::var("SOURCE_DATE_EPOCH").unwrap_or_else(|_| {
+        rand::rng()
             .sample_iter(Alphanumeric)
             .take(8)
             .map(char::from)
-            .collect(),
-    };
+            .collect()
+    });
 
     println!("cargo:rustc-env=LIBRESPOT_BUILD_ID={build_id}");
     Ok(())
